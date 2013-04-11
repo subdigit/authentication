d4809a47194d1f0b2f98da4e04dc67d87c2f4fe5
subdigit <subdigit@gmail.com>
2013-04-11 14:10:11 -0400

    added persona entry to the sample property file

--- src/main/java/com/subdigit/auth/conf/AuthenticationServiceConfiguration.java
+++ src/main/java/com/subdigit/auth/conf/AuthenticationServiceConfiguration.java
@@ -17,26 +17,28 @@ import com.subdigit.auth.service.AuthenticationService;
 
 public class AuthenticationServiceConfiguration
 {
-	public static final String PREFIX							= "asc";
-	public static final String FILE								= "authenticationserviceconfiguration.properties";
+	public static final String PREFIX								= "asc";
+	public static final String FILE									= "authenticationserviceconfiguration.properties";
 
-	public static final String PROPERTY_AVAILABLESERVICES		= PREFIX + ".availableservices";
-	public static final String PROPERTY_SERVICE_ENABLED			= PREFIX + ".service.%s.enabled";
-	public static final String PROPERTY_SERVICE_NAME			= PREFIX + ".service.%s.name";
-	public static final String PROPERTY_SERVICE_CLASSNAME		= PREFIX + ".service.%s.classname";
-	public static final String PROPERTY_SERVICE_APPID			= PREFIX + ".service.%s.appid";
-	public static final String PROPERTY_SERVICE_APPSECRET		= PREFIX + ".service.%s.appsecret";
-	public static final String PROPERTY_SERVICE_SCOPE			= PREFIX + ".service.%s.scope";
-	public static final String PROPERTY_SERVICE_STATECHECK		= PREFIX + ".service.%s.statecheck";
+	public static final String PROPERTY_AVAILABLESERVICES			= PREFIX + ".availableservices";
+	public static final String PROPERTY_SERVICE_ENABLED				= PREFIX + ".service.%s.enabled";
+	public static final String PROPERTY_SERVICE_NAME				= PREFIX + ".service.%s.name";
+	public static final String PROPERTY_SERVICE_CLASSNAME			= PREFIX + ".service.%s.classname";
+	public static final String PROPERTY_SERVICE_APPID				= PREFIX + ".service.%s.appid";
+	public static final String PROPERTY_SERVICE_APPSECRET			= PREFIX + ".service.%s.appsecret";
+	public static final String PROPERTY_SERVICE_CLIENTHANDLEDLOGIN	= PREFIX + ".service.%s.clienthandledlogin";
+	public static final String PROPERTY_SERVICE_SCOPE				= PREFIX + ".service.%s.scope";
+	public static final String PROPERTY_SERVICE_STATECHECK			= PREFIX + ".service.%s.statecheck";
 
-	public static final List<String> DEFAULT_AVAILABLESERVICES	= null;
-	public static final boolean DEFAULT_SERVICE_ENABLED			= false;
-	public static final String DEFAULT_SERVICE_NAME				= null;
-	public static final String DEFAULT_SERVICE_CLASSNAME		= null;
-	public static final String DEFAULT_SERVICE_APPID			= null;
-	public static final String DEFAULT_SERVICE_APPSECRET		= null;
-	public static final List<String> DEFAULT_SERVICE_SCOPE		= null;
-	public static final boolean DEFAULT_SERVICE_STATECHECK		= false;
+	public static final List<String> DEFAULT_AVAILABLESERVICES		= null;
+	public static final boolean DEFAULT_SERVICE_ENABLED				= false;
+	public static final String DEFAULT_SERVICE_NAME					= null;
+	public static final String DEFAULT_SERVICE_CLASSNAME			= null;
+	public static final String DEFAULT_SERVICE_APPID				= null;
+	public static final String DEFAULT_SERVICE_APPSECRET			= null;
+	public static final boolean DEFAULT_SERVICE_CLIENTHANDLEDLOGIN	= false;
+	public static final List<String> DEFAULT_SERVICE_SCOPE			= null;
+	public static final boolean DEFAULT_SERVICE_STATECHECK			= false;
 
 	private static AuthenticationServiceConfiguration _authenticationServiceConfiguration;
 
@@ -81,6 +83,7 @@ public class AuthenticationServiceConfiguration
 				if(serviceData.newInstance() == null) continue;
 				serviceData.setAppID(config.getString(String.format(PROPERTY_SERVICE_APPID, serviceName)));
 				serviceData.setAppSecret(config.getString(String.format(PROPERTY_SERVICE_APPSECRET, serviceName)));
+				serviceData.setClientHandledLogin(config.getBoolean(String.format(PROPERTY_SERVICE_CLIENTHANDLEDLOGIN, serviceName)));
 				String[] scopes = config.getStringArray(String.format(PROPERTY_SERVICE_SCOPE, serviceName));
 				if(scopes != null && scopes.length > 0 && !StringUtils.isBlank(scopes[0])) serviceData.setScope(Arrays.asList(scopes));
 				serviceData.setStateCheck(config.getBoolean(String.format(PROPERTY_SERVICE_STATECHECK, serviceName)));
@@ -140,6 +143,12 @@ public class AuthenticationServiceConfiguration
 		if(serviceData != null) return serviceData.getAppSecret();
 		else return DEFAULT_SERVICE_APPSECRET;
 	}
+	public boolean getServiceClientHandledLogin(String service)
+	{
+		ServiceData serviceData = getServiceData(service);
+		if(serviceData != null) return serviceData.getClientHandledLogin();
+		else return DEFAULT_SERVICE_CLIENTHANDLEDLOGIN;
+	}
 	public List<String> getServiceScope(String service)
 	{
 		ServiceData serviceData = getServiceData(service);
@@ -175,6 +184,7 @@ public class AuthenticationServiceConfiguration
 		private String _className;
 		private String _appID;
 		private String _appSecret;
+		private boolean _clientHandledLogin;
 		private List<String> _scope;
 		private boolean _stateCheck;
 
@@ -190,6 +200,7 @@ public class AuthenticationServiceConfiguration
 			_className = DEFAULT_SERVICE_CLASSNAME;
 			_appID = DEFAULT_SERVICE_APPID;
 			_appSecret = DEFAULT_SERVICE_APPSECRET;
+			_clientHandledLogin = DEFAULT_SERVICE_CLIENTHANDLEDLOGIN;
 			_scope = DEFAULT_SERVICE_SCOPE;
 			_stateCheck = DEFAULT_SERVICE_STATECHECK;
 			
@@ -211,6 +222,9 @@ public class AuthenticationServiceConfiguration
 
 		public String getAppSecret(){ return _appSecret; }
 		public void setAppSecret(String value){ _appSecret = value; }
+
+		public boolean getClientHandledLogin(){ return _clientHandledLogin; }
+		public void setClientHandledLogin(boolean value){ _clientHandledLogin = value; }
 
 		public List<String> getScope(){ return _scope; }
 		public void setScope(List<String> value){ _scope = value; }
